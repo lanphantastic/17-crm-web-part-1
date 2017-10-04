c63356ba8a1da5c3248e20fc05e4015d28fab282
Lan Phan <lanc_phan@hotmail.com>
2017-10-04 14:42:45 -0400

    added contacts.erb to view the contacts I have added through the CRM interface

--- crm.rb
+++ crm.rb
@@ -4,3 +4,13 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @all_contacts = Contact.all
+  
+  erb :contacts
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
