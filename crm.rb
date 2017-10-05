361f5af96444fc8a88b7415cb1006ee9f5b0a1cb
Lan Phan <lanc_phan@hotmail.com>
2017-10-05 15:25:41 -0400

    return a 404 Not Found response status code if we can't find what the user is asking for

--- crm.rb
+++ crm.rb
@@ -20,7 +20,14 @@ get '/contacts/:id' do
   # instructions for how to handle requests to this route will go here
   # params[:id] contains the id from the URL
   @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+
+  # returns a 404 Not Found response status code if we can't find what the user is asking for
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+
 end
 
 
